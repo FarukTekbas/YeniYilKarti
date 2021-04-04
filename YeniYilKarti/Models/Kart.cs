8727f6b672b5db09efb5850388cebf175bef5e83
Faruk Tekbas <faruktekbas60@gmail.com>
2021-04-05 01:58:41 +0300

    Add project files.

---
+++ YeniYilKarti/Models/Kart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace YeniYilKarti.Models
+{
+    [Table("Kartlar)")]
+    public class Kart
+    {
+        public int Id { get; set; }
+
+        public string Baslik { get; set; }
+        public string GonderenAd { get; set; }
+        [Required]
+        public string AliciAd { get; set; }
+        [Required]
+        [MaxLength(55)]
+        public string Mesaj1 { get; set; }
+        [MaxLength(55)]
+        public string Mesaj2 { get; set; }
+        [MaxLength(55)]
+        public string Mesaj3 { get; set; }
+        [Required]
+        public string ResimAd { get; set; }
+        [ForeignKey("User")]
+        public virtual string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
